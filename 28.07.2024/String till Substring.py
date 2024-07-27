39755ac475aab5f18e03e6860133d30da0006912
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2024-07-27 14:34:19 +0530

    String till Substring.py

---
+++ 28.07.2024/String till Substring.py
@@ -0,0 +1,11 @@
+test_string = "GeeksforGeeks is best for geeks"
+
+spl_word = 'best'
+
+print("The original string : " + str(test_string))
+
+print("The split string : " + str(spl_word))
+
+res = test_string.partition(spl_word)[0]
+
+print("String before the substring occurrence : " + res)
