21a67bc93bbf1a59d44525024774160e6f7ebf8f
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2024-07-27 13:50:11 +0530

    calculate cube root.py

---
+++ 27.07.2024/calculate cube root.py
@@ -0,0 +1,10 @@
+def calculate_cube_root(x): 
+	return x**(1/3) 
+print(calculate_cube_root(27)) 
+languages = ['Sanskrut', 'English', 'French', 'German'] 
+
+def check_language(x): 
+	if x in languages: 
+		return True
+	return False
+print(check_language('English')) 
