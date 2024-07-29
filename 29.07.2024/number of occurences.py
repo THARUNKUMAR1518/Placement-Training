d424146c145ff9c1704d0100c82932872fab4974
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2024-07-29 22:17:36 +0530

    Create number of occurences.py

---
+++ 29.07.2024/number of occurences.py
@@ -0,0 +1,4 @@
+elements = ['a', 'b', 'a', 'c', 'a', 'b']
+element_to_count = 'a'
+count = elements.count(element_to_count)
+print(f"The element '{element_to_count}' appears {count} times in the list.")
