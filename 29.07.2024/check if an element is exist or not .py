174e17744632de07443d006b129fde5199dffc3f
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2024-07-29 22:15:07 +0530

    Create check if an element is exist or not .py

---
+++ 29.07.2024/check if an element is exist or not .py
@@ -0,0 +1,6 @@
+elements = ['apple', 'banana', 'cherry', 'date']
+element_to_check = 'banana'
+if element_to_check in elements:
+    print(f"The element '{element_to_check}' exists in the list.")
+else:
+    print(f"The element '{element_to_check}' does not exist in the list.")
